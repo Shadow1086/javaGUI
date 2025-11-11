69b37d1beda93cd3932c1f1869dad15ffa203771
Liang-ht <l15304704058@126.com>
2025-11-11 20:59:47 +0800

    ✨ feat(BorderTest): 新增 BorderTest 类，演示 Swing 中的边框功能 - 创建 BorderTest 类以学习和展示 Swing 中的边框使用 - 初始化 JFrame 窗口，准备实现网格布局

---
+++ src/main/java/Swing/b_BorderShowcase/BorderTest.java
@@ -0,0 +1,38 @@
+package Swing.b_BorderShowcase;
+import javax.swing.*;
+
+/**
+ * ClassName: BorderTest
+ * Description: 
+ * 
+ * Swing中的Border学习
+ * 
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025-11-11 18:22:56
+ */
+public class BorderTest {
+    JFrame frame = new JFrame("测试边框");
+
+    public void init(){
+        //组装视图
+        //1. 窗口JFrame的布局修改为网格布局
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        new BorderTest().init();
+    }
+}
+
+
