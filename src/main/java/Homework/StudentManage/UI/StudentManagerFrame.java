63c5e5b33777f138c7e14e05844a9146d04edf8c
Liang-ht <l15304704058@126.com>
2025-11-18 23:48:33 +0800

    feat: 实现学生信息表格显示功能
    - 添加JTable组件展示学生基本信息（编号、姓名、性别、学号、年龄、创建时间）
    - 使用DefaultTableModel管理表格数据，支持动态添加学生记录
    - 集成滚动面板提升大数据量显示体验
    未完成项：获取用户输入的信息来添加到表格中

--- src/main/java/Homework/StudentManage/UI/StudentManagerFrame.java
+++ src/main/java/Homework/StudentManage/UI/StudentManagerFrame.java
@@ -1,11 +1,15 @@
 package Homework.StudentManage.UI;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
+import java.util.Vector;
+
 /**
  * ClassName: StudentManagerFrame
- * Description: 
+ * Description:
  * 
  * 主窗口
  * 
@@ -14,67 +18,98 @@ import java.io.File;
  */
 public class StudentManagerFrame {
     JFrame frame = new JFrame("学生管理系统");
-    
-    //声明菜单相关组件
+
+    // 声明菜单相关组件
     JMenuBar menuBar = new JMenuBar();
 
     JMenu file = new JMenu("文件");
-    JMenuItem save = new JMenuItem(new AbstractAction("导出"){
+    JMenuItem save = new JMenuItem(new AbstractAction("导出") {
         @Override
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             JFileChooser fileChooser = new JFileChooser(".");
             fileChooser.showSaveDialog(frame);
             File file = fileChooser.getSelectedFile();
             try {
-                //TODO:handle the file
+                // TODO:handle the file
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
         }
 
     });
-    JMenuItem  open = new JMenuItem(new AbstractAction("导入") {
-        @Override 
-        public void actionPerformed(ActionEvent e){
+    JMenuItem open = new JMenuItem(new AbstractAction("导入") {
+        @Override
+        public void actionPerformed(ActionEvent e) {
             JFileChooser fileChooser = new JFileChooser(".");
             fileChooser.showOpenDialog(frame);
             File file = fileChooser.getSelectedFile();
             try {
-                //TODO: handle the file
+                // TODO: handle the file
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
         }
     });
 
-
-
-    //声明右键
+    // 声明右键
     ButtonGroup popupButtonGroup = new ButtonGroup();
     JRadioButtonMenuItem delete = new JRadioButtonMenuItem("删除所选行");
-    JRadioButtonMenuItem change =  new JRadioButtonMenuItem("修改数据");
+    JRadioButtonMenuItem change = new JRadioButtonMenuItem("修改数据");
 
+    // 表格
+    // 表头（列名）
+    Vector<String> columnNames = new Vector<String>() {
+        {
+            add("编号");
+            add("姓名");
+            add("性别");
+            add("学号");
+            add("年龄");
+            add("创建时间");
+        }
+    };
+    // 创建表格模型
+    DefaultTableModel dataModal = new DefaultTableModel(columnNames, 0);
+    // 创建JTable表格组件
+    JTable table = new JTable(dataModal);
+    // 创建带滚动条的面板，并将表格添加到带有滚动条的面板中
+    JScrollPane scorllPane = new JScrollPane(table);
+    // 将表头添加到面板中（布局的上方）
+    // 创建一个容器实例
+    JPanel tableBoxPanel = new JPanel(new BorderLayout());
 
+    // 创建数据的表格
+    String[][] tableValue = {
+            { "01", "ceshi", "nv", "28390e", "28", "asljkdf" }
+    };
 
+    // 定义一个方法来添加学生数据
+    public void addStudent(String[][] tableValue) {
+        for (String[] arr : tableValue) {
+            dataModal.addRow(arr);
+        }
 
+    }
 
+    // 组装组件
+    public void init() {
+        // 组装表格面板
+        tableBoxPanel.add(table.getTableHeader(), BorderLayout.NORTH);
+        tableBoxPanel.add(scorllPane, BorderLayout.CENTER);
 
-    
-    //组装组件
-    public void init(){
-        //组装菜单栏
+        // 组装菜单栏
         menuBar.add(file);
 
         file.add(save);
         file.add(open);
 
         frame.setJMenuBar(menuBar);
-        //组装右键
+        // 组装右键
         popupButtonGroup.add(change);
         popupButtonGroup.add(delete);
 
 
-
+        frame.add(tableBoxPanel);
         frame.setPreferredSize(new Dimension(800, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
