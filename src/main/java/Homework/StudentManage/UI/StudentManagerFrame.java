e16d3d3501fdbaed3c56803c46152bd07286871a
Liang-ht <l15304704058@126.com>
2025-11-19 14:07:05 +0800

    feat: 补全了右键功能，底部输入信息区域基本完成，待办：底部提示信息以及按钮相应的功能
    - 添加底部输入面板组件(姓名、学号、性别、年龄输入框及确定/取消按钮)
    - 实现右键菜单的删除和修改功能监听器
    - 为表格添加鼠标右键菜单显示逻辑

--- src/main/java/Homework/StudentManage/UI/StudentManagerFrame.java
+++ src/main/java/Homework/StudentManage/UI/StudentManagerFrame.java
@@ -23,52 +23,17 @@ public class StudentManagerFrame {
     JMenuBar menuBar = new JMenuBar();
 
     JMenu file = new JMenu("文件");
-    JMenuItem save = new JMenuItem(new AbstractAction("导出") {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JFileChooser fileChooser = new JFileChooser(".");
-            fileChooser.showSaveDialog(frame);
-            File file = fileChooser.getSelectedFile();
-            try {
-                // TODO:handle the file
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        }
+    JMenuItem save=new JMenuItem(new AbstractAction("导出"){@Override public void actionPerformed(ActionEvent e){JFileChooser fileChooser=new JFileChooser(".");fileChooser.showSaveDialog(frame);File file=fileChooser.getSelectedFile();try{
+    // TODO:handle the file
+    }catch(Exception exception){exception.printStackTrace();}}
 
     });
-    JMenuItem open = new JMenuItem(new AbstractAction("导入") {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JFileChooser fileChooser = new JFileChooser(".");
-            fileChooser.showOpenDialog(frame);
-            File file = fileChooser.getSelectedFile();
-            try {
-                // TODO: handle the file
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        }
-    });
+    JMenuItem open=new JMenuItem(new AbstractAction("导入"){@Override public void actionPerformed(ActionEvent e){JFileChooser fileChooser=new JFileChooser(".");fileChooser.showOpenDialog(frame);File file=fileChooser.getSelectedFile();try{
+    // TODO: handle the file
+    }catch(Exception exception){exception.printStackTrace();}}});
 
-    // 声明右键
-    JPopupMenu popupMenu = new JPopupMenu();
-    ButtonGroup popupButtonGroup = new ButtonGroup();
-    JRadioButtonMenuItem delete = new JRadioButtonMenuItem("删除所选行");
-    JRadioButtonMenuItem change = new JRadioButtonMenuItem("修改数据");
-
-    // 表格
     // 表头（列名）
-    Vector<String> columnNames = new Vector<String>() {
-        {
-            add("编号");
-            add("姓名");
-            add("性别");
-            add("学号");
-            add("年龄");
-            add("创建时间");
-        }
-    };
+    Vector<String> columnNames=new Vector<String>(){{add("编号");add("姓名");add("性别");add("学号");add("年龄");add("创建时间");}};
     // 创建表格模型
     DefaultTableModel dataModal = new DefaultTableModel(columnNames, 0);
     // 创建JTable表格组件
@@ -89,12 +54,24 @@ public class StudentManagerFrame {
         for (String[] arr : tableValue) {
             dataModal.addRow(arr);
         }
-
     }
+     // 声明右键
+    JPopupMenu popupMenu = new JPopupMenu();
+    ButtonGroup popupButtonGroup = new ButtonGroup();
+    JRadioButtonMenuItem delete = new JRadioButtonMenuItem("删除所选行");
+    JRadioButtonMenuItem change = new JRadioButtonMenuItem("修改数据");
 
+//声明底部
+    JTextField tfName = new JTextField(10);
+    JTextField tfStuId = new JTextField(10);
+    JTextField tfSex = new JTextField(10);
+    JTextField tfAge = new JTextField(10);
+
+    JButton cencel = new JButton("取消");
+    JButton ok = new JButton("确定");
     // 组装组件
     public void init() {
-        //添加数据
+        // 添加数据
         addStudent(tableValue);
         // 组装表格面板
         tableBoxPanel.add(table.getTableHeader(), BorderLayout.NORTH);
@@ -107,12 +84,55 @@ public class StudentManagerFrame {
         file.add(open);
 
         frame.setJMenuBar(menuBar);
+        //组装底部
+        JPanel bottomPane = new JPanel();
+        bottomPane.add(tfName);
+        bottomPane.add(tfStuId);
+        bottomPane.add(tfSex);
+        bottomPane.add(tfAge);
+        bottomPane.add(cencel);
+        bottomPane.add(ok);
+        frame.add(bottomPane,BorderLayout.SOUTH);
         // 组装右键
         popupButtonGroup.add(delete);
         popupButtonGroup.add(change);
         popupMenu.add(change);
         popupMenu.add(delete);
+        // 给表格添加右键菜单监听器
+            delete.addActionListener(new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e){
+            int selectedRow = table.getSelectedRow();
+            if(selectedRow != -1){
+                dataModal.removeRow(selectedRow);
+            }else{
+                JOptionPane.showMessageDialog(frame,"请选中要删除的行");
+            }
+        }
+    });
+    change.addActionListener(new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e){
+            int row = table.getSelectedRow();
+            if(row != -1){
+                //TODO：补全修改功能
+            }
+        }
+    });
+        table.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                if (e.isPopupTrigger())
+                    showMenu(e);
+            }
 
+            private void showMenu(MouseEvent e) {
+                int row = table.rowAtPoint(e.getPoint());
+                if (row >= 0) {
+                    table.setRowSelectionInterval(row, row);
+                }
+                popupMenu.show(e.getComponent(), e.getX(), e.getY());
+            }
+        });
 
         frame.add(tableBoxPanel);
         frame.setPreferredSize(new Dimension(800, 600));
